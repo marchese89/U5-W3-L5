46efb43f5d8616071f358e7353d316f22893c415
marchese89 <marchese89@hotmail.com>
2024-01-26 09:20:15 +0100

    Initial commit

---
+++ src/main/java/antoniogiovanni/marchese/U5W3L5/U5W3L5Application.java
@@ -0,0 +1,13 @@
+package antoniogiovanni.marchese.U5W3L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/antoniogiovanni/marchese/U5W3L5/U5W3L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package antoniogiovanni.marchese.U5W3L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
